058f2be947049cff8118a8d8de169f9b07ac4d6a
hanna <hannahranchenko@gmail.com>
2017-08-14 11:15:10 +0300

    'Jquery-part1'

---
+++ static/static_dev/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var form = $('#form_buying_product');
+    console.log(form);
+    form.on('submit', function(e){
+        e.preventDefault();
+        console.log('123');
+        var nmb = $('#number').val();
+        console.log(nmb);
+
+    })
+
+});
