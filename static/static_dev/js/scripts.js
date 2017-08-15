b653fb0ca7ab91955c5e1ed1b36db6d73a56267b
hanna <hannahranchenko@gmail.com>
2017-08-15 15:10:49 +0300

    Jquery-part1.1

--- static/static_dev/js/scripts.js
+++ static/static_dev/js/scripts.js
@@ -6,7 +6,25 @@ $(document).ready(function(){
         console.log('123');
         var nmb = $('#number').val();
         console.log(nmb);
+        var submit_btn = $('#submit_btn');
+        var product_id = submit_btn.data("product_id");
+        var product_name = submit_btn.data("name");
+        var price = submit_btn.data("price") // data-атрибуты добавляем в product.html 
+        console.log(product_id);
+        console.log(name);
+    })
 
+    $('.basket-container').on('click', function(e){
+    	e.preventDefault();
+    	$('.basket-items').removeClass('hidden');
+    })
+
+    $('.basket-container').mouseover(function(){
+    	$('.basket-items').removeClass('hidden');
+    })
+
+    $('.basket-container').mouseout(function(){
+    	$('.basket-items').addClass('hidden');
     })
 
 });
