3555e15decbca09d2d7c095a21acd5a38aa1cdba
hanna <hannahranchenko@gmail.com>
2017-08-18 17:10:49 +0300

    Ajax-part2,3

--- static/static_dev/js/scripts.js
+++ static/static_dev/js/scripts.js
@@ -1,49 +1,65 @@
 $(document).ready(function(){
     var form = $('#form_buying_product');
     console.log(form);
+
+
+function basketUpdating(product_id, nmb, is_delete){
+        var data = {};
+        data.product_id = product_id;
+        data.nmb = nmb;
+        var csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
+        data["csrfmiddlewaretoken"] = csrf_token; //csrf_token помогает django делать запрос
+
+        if (is_delete){
+            data["is_delete"] = true;
+        }
+
+        var url = form.attr("action");
+
+        console.log(data)
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: data,
+            cache: true,
+            success: function (data) {
+                console.log("OK");
+                console.log(data.products_total_nmb);
+                if (data.products_total_nmb || data.products_total_nmb == 0){
+                    $('#basket_total_nmb').text("("+data.products_total_nmb+")");
+                    console.log(data.products);
+                    $('.basket-items ul').html("");
+                    $.each(data.products, function(k, v){
+                        $('.basket-items ul').append('<li>'+ v.name+', ' + v.nmb + 'шт. ' + 'по ' + v.price_per_item + 'грн  ' +
+                            '<a class="delete-item" href="" data-product_id="'+v.id+'">x</a>'+
+                            '</li>');
+                    });
+                }
+
+            },
+            error: function(){
+                console.log("error")
+            }
+        })
+
+    }
+
     form.on('submit', function(e){
         e.preventDefault();
         console.log('123');
         var nmb = $('#number').val();
         console.log(nmb);
         var submit_btn = $('#submit_btn');
-        var product_id = submit_btn.data("product_id");
+        var product_id =  submit_btn.data("product_id");
         var name = submit_btn.data("name");
-        var price = submit_btn.data("price") // data-атрибуты добавляем в product.html 
-        console.log(product_id);
+        var price = submit_btn.data("price");  //дата-атрибуты добавляем в product.html
+        console.log(product_id );
         console.log(name);
 
-            var data = {};
-            data.product_id = product_id;
-            data.nmb = nmb;
-            var csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
-            data["csrfmiddlewaretoken"] = csrf_token;  //csrf_token помогает django делать запрос
-            
-            var url = form.attr("action");
-
-            console.log(data)
-            $.ajax({
-                url: url,
-                type: 'POST',
-                data: data,
-                cache: true,
-                success: function (data) {
-                    console.log("OK");
-                    console.log(data.products_total_nmb);
-                    if (data.products_total_nmb){
-                        $('#basket_total_nmb').text("("+data.products_total_nmb+")")
-                    }
-                },
-                error: function(){
-                    console.log("error");
-                }
-            })
-
-        $('.basket-items ul').append('<li>'+name+', ' + nmb +
-         'шт. ' + 'по ' + price + 'грн  ' + '<a class="delete-item" href="">x</a>' + 
-         '</li>');
+        basketUpdating(product_id, nmb, is_delete=false)
 
     });
+
 
     function showingBasket(){
         $('.basket-items').removeClass('hidden');//корзина в открытом виде
@@ -66,8 +82,11 @@ $(document).ready(function(){
 
     $(document).on('click', '.delete-item', function(e){
         e.preventDefault();
-        $(this).closest('li').remove();
+        //$(this).closest('li').remove();
         //для удаления из корзины
+        product_id = $(this).data("product_id")
+        nmb = 0;
+        basketUpdating(product_id, nmb, is_delete=true)
     })
 
 });
