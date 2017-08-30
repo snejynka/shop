c2beafa97d71522d32738be703f820796b7f00ab
hanna <hannahranchenko@gmail.com>
2017-08-30 14:31:24 +0300

    Some adding

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,8 +130,11 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, "static",),
+  os.path.join(BASE_DIR, "static"),
 )
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_prod")
 
 MEDIA_URL = '/media/'
 
